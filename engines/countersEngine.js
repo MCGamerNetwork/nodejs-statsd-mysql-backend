54d0af1ed99718d0eb87a8c05f41920db328154a
Gideon Beebe <gjosiah7@gmail.com>
2014-12-18 14:48:16 -0500

    Update countersEngine.js to send some statistics to other tables

--- engines/countersEngine.js
+++ engines/countersEngine.js
@@ -27,7 +27,25 @@ MySQLBackendCountersEngine.prototype.buildQuerries = function(userCounters, time
          *    - userCounterName: Counter name
          *    - counterValue: Counter value
          */
-        querries.push("insert into `counters_statistics` select "+time_stamp+", '"+userCounterName+"', if(max(absolute),max(absolute),0) + "+counterValue+", "+counterValue+" from `counters_statistics`  where if(name = '"+userCounterName+"', 1,0) = 1 ;");
+        if (userCounterName.search("stats\\.game\\.*\\.") >= 0) {
+          // Mirror to separate tables if the metric is a game won/lost/played statistic
+          if (userCounterName.indexOf("(won|lost|played)") >= 0) {
+            var game = userCounterName.substring(12);
+            game = game.substring(0, game.indexOf("."));
+
+            var pid = userCounterName.substring(userCounterName.lastIndexOf(".") + 1);
+
+            if (userCounterName.indexOf("lost") >= 0) {
+              querries.push("insert into `"+game+"_lost` select "+time_stamp+", "+pid+", if(max(absolute),max(absolute),0) + "+counterValue+", "+counterValue+" from `"+game+"_lost` where if(id = "+pid+", 1,0) = 1;");
+            } else if (userCounterName.indexOf("played") >= 0) {
+              querries.push("insert into `"+game+"_played` select "+time_stamp+", "+pid+", if(max(absolute),max(absolute),0) + "+counterValue+", "+counterValue+" from `"+game+"_played` where if(id = "+pid+", 1,0) = 1;");
+            } else if (userCounterName.indexOf("won") >= 0) {
+              querries.push("insert into `"+game+"_won` select "+time_stamp+", "+pid+", if(max(absolute),max(absolute),0) + "+counterValue+", "+counterValue+" from `"+game+"_won` where if(id = "+pid+", 1,0) = 1;");
+            }
+          }
+        }
+
+        querries.push("insert into `counters_statistics` select "+time_stamp+", '"+userCounterName+"', if(max(absolute),max(absolute),0) + "+counterValue+", "+counterValue+" from `counters_statistics` where if(name = '"+userCounterName+"', 1,0) = 1;");
       }
     }
 
